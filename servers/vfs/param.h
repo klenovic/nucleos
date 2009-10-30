05abc25d013462ce7d449ef006de6e72ad473cf6
Ladislav Klenovic <klenovic@nucleonsoft.com>
2009-10-30 15:57:19 +0100

    Patches from minix3
    	* The minix3 patches:
    	  branch: trunk
    	  revision range: 5568-5575
    	  applied patches (SVN log):
    		------------------------------------------------------------------------
    		r5569 | dcvmoole | 2009-10-29 14:29:04 +0100 (Thu, 29 Oct 2009) | 1 line
    
    		PM, VFS: remove unused param.h definitions
    		------------------------------------------------------------------------

--- servers/vfs/param.h
+++ servers/vfs/param.h
@@ -11,19 +11,13 @@
 #define __SERVERS_VFS_PARAM_H
 
 /* The following names are synonyms for the variables in the input message. */
-#define acc_time      m2_l1
 #define addr	      m1_i3
 #define buffer	      m1_p1
 #define child_endpt      m1_i2
 #define co_mode	      m1_i1
-#define eff_grp_id    m1_i3
-#define eff_user_id   m1_i3
-#define erki          m1_p1
 #define fd	      m1_i1
 #define fd2	      m1_i2
-#define ioflags       m1_i3
 #define group	      m1_i3
-#define real_grp_id   m1_i2
 #define ls_fd	      m2_i1
 #define mk_mode	      m1_i2
 #define mk_z0	      m1_i3
@@ -38,7 +32,6 @@
 #define name2_length  m1_i2
 #define nbytes        m1_i2
 #define owner	      m1_i2
-#define parent_endpt      m1_i1
 #define pathname      m3_ca1
 #define pid	      m1_i3
 #define ENDPT	      m1_i1
@@ -48,7 +41,6 @@
 #define dev_style     m4_l4
 #define m_force	      m4_l5
 #define rd_only	      m1_i3
-#define real_user_id  m1_i2
 #define request       m1_i2
 #define sig	      m1_i2
 #define endpt1	      m1_i1
@@ -61,7 +53,6 @@
 #define whence	      m2_i2
 #define svrctl_req    m2_i1
 #define svrctl_argp   m2_p1
-#define pm_stime      m1_i1
 #define info_what     m1_i1
 #define info_where    m1_p1
 #define md_label	m2_p1
@@ -76,10 +67,5 @@
 #define reply_l2      m2_l2
 #define reply_i1      m1_i1
 #define reply_i2      m1_i2
-#define reply_t1      m4_l1
-#define reply_t2      m4_l2
-#define reply_t3      m4_l3
-#define reply_t4      m4_l4
-#define reply_t5      m4_l5
 
 #endif /* __SERVERS_VFS_PARAM_H */
