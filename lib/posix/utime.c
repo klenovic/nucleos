a452e1162ca93b1beb5f385be1eed2903b263433
Ladislav Klenovic <klenovic@nucleonsoft.com>
2009-11-26 15:45:05 +0100

    Delete old dead `utime' code.

--- lib/posix/utime.c
+++ lib/posix/utime.c
@@ -17,21 +17,3 @@ int utime(const char *filename, const struct utimbuf *times)
 {
 	return INLINE_SYSCALL(utime, 2, filename, times);
 }
-
-#if 0
-int utime(const char *name, const struct utimbuf *timp)
-{
-  message m;
-
-  if (timp == NULL) {
-	m.m2_i1 = 0;		/* name size 0 means NULL `timp' */
-	m.m2_i2 = strlen(name) + 1;	/* actual size here */
-  } else {
-	m.m2_l1 = timp->actime;
-	m.m2_l2 = timp->modtime;
-	m.m2_i1 = strlen(name) + 1;
-  }
-  m.m2_p1 = (char *) name;
-  return(ksyscall(FS_PROC_NR, __NR_utime, &m));
-}
-#endif
