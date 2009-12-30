d000d3d1a2d592ff00999dd53fb49db504517733
Ladislav Klenovic <klenovic@nucleonsoft.com>
2009-12-30 16:18:44 +0100

    Remove unused headers

--- lib/posix/munmap.c
+++ lib/posix/munmap.c
@@ -7,10 +7,9 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, version 2 of the License.
  */
-#include <nucleos/lib.h>
+#include <nucleos/unistd.h>
 #include <nucleos/mman.h>
-#include <nucleos/string.h>
-#include <nucleos/errno.h>
+#include <asm/syscall.h>
 #include <stdarg.h>
 
 int __munmap(void *addr, size_t len)
