45f9c30282f6ef449d3edcc2842c7cc28bc43aa7
Ladislav Klenovic <klenovic@nucleonsoft.com>
2010-01-13 12:33:31 +0100

    Adjust signal.h headers and add new signal definitions
    	* Add SIGXCPU and SIGXFSZ. They are still not supported.
    	* Add SIGCLD and set to SIGCHILD

--- arch/x86/include/asm/signal.h
+++ arch/x86/include/asm/signal.h
@@ -14,12 +14,13 @@
 #ifndef __ASM_X86_SIGNAL_H
 #define __ASM_X86_SIGNAL_H
 
+#ifndef __ASSEMBLY__
 #include <nucleos/types.h>
 
 #if defined(__KERNEL__) || defined(__UKERNEL__)
-#define _NSIG	26	/* highest signal number plus one */
+#define _NSIG	29	/* highest signal number plus one */
 #else
-#define NSIG	26
+#define NSIG	29
 #endif
 
 /* Here are types that are closely associated with signal handling. */
@@ -28,6 +29,7 @@ typedef int sig_atomic_t;
 #endif /* defined(__KERNEL__) || defined(__UKERNEL__) */
 
 typedef unsigned long sigset_t;
+#endif /* __ASSEMBLY__ */
 
 /* Regular signals. */
 #define SIGHUP		1	/* hangup */
@@ -36,6 +38,7 @@ typedef unsigned long sigset_t;
 #define SIGILL		4	/* illegal instruction */
 #define SIGTRAP		5	/* trace trap (not reset when caught) */
 #define SIGABRT		6	/* IOT instruction */
+#define SIGIOT		6	/* IOT trap */
 #define SIGBUS		7	/* bus error */
 #define SIGFPE		8	/* floating point exception */
 #define SIGKILL		9	/* kill (cannot be caught or ignored) */
@@ -46,30 +49,21 @@ typedef unsigned long sigset_t;
 #define SIGALRM		14	/* alarm clock */
 #define SIGTERM		15	/* software termination signal from kill */
 #define SIGEMT		16	/* EMT instruction */
+#define SIGCLD		SIGCHLD	/* child process terminated or stopped */
 #define SIGCHLD		17	/* child process terminated or stopped */
-#define SIGWINCH	21	/* window size has changed */
+#define SIGCONT		18	/* continue if stopped (n/a) */
+#define SIGSTOP		19	/* stop signal (n/a) */
+#define SIGTSTP		20	/* interactive stop signal (n/a) */
+#define SIGWINCH	21	/* window size has changed (n/a) */
+#define SIGTTIN		22	/* background process wants to read (n/a) */
+#define SIGTTOU		23	/* background process wants to write (n/a) */
 #define SIGVTALRM	24	/* virtual alarm */
 #define SIGPROF		25	/* profiler alarm */
+#define SIGXCPU		26	/* CPU limit exceeded (4.2 BSD) (n/a) */
+#define SIGXFSZ		27	/* File size limit exceeded (4.2 BSD) (n/a) */
 
-/* POSIX requires the following signals to be defined, even if they are
- * not supported.  Here are the definitions, but they are not supported.
- */
-#define SIGCONT		18	/* continue if stopped */
-#define SIGSTOP		19	/* stop signal */
-#define SIGTSTP		20	/* interactive stop signal */
-#define SIGTTIN		22	/* background process wants to read */
-#define SIGTTOU		23	/* background process wants to write */
-
-#define SIGIOT		SIGABRT	/* for people who speak PDP-11 */
-
-#if defined(__KERNEL__) || defined(__UKERNEL__)
-/* Nucleos specific signals. These signals are not used by user proceses, 
- * but meant to inform system processes, like the PM, about system events.
- */
-#define SIGKMESS	29	/* new kernel message */
-#define SIGKSIG		30	/* kernel signal pending */
-#define SIGNDELAY	31	/* kernel shutting down */
-#endif /* defined(__KERNEL__) || defined(__UKERNEL__) */
+#define SIGSYS		28	/* bad argument to a system call (n/a) */
+#define SIGUNUSED	28	/* purpose at present but is intended for future use (n/a) */
 
 /* Fields for sa_flags. */
 #define SA_ONSTACK	0x0001	/* deliver signal on alternate stack */
@@ -80,12 +74,23 @@ typedef unsigned long sigset_t;
 #define SA_NOCLDWAIT	0x0020	/* don't create zombies */
 #define SA_NOCLDSTOP	0x0040	/* don't receive SIGCHLD when child stops */
 
+#if defined(__KERNEL__) || defined(__UKERNEL__)
+/* Nucleos specific signals. These signals are not used by user proceses, 
+ * but meant to inform system processes, like the PM, about system events.
+ */
+#define SIGKMESS	29	/* new kernel message */
+#define SIGKSIG		30	/* kernel signal pending */
+#define SIGNDELAY	31	/* kernel shutting down */
+#endif /* defined(__KERNEL__) || defined(__UKERNEL__) */
+
 #include <asm-generic/signal-defs.h>
 
+#ifndef __ASSEMBLY__
 struct sigaction {
 	__sighandler_t sa_handler;	/* SIG_DFL, SIG_IGN, or pointer to function */
 	sigset_t sa_mask;		/* signals to be blocked during handler */
 	int sa_flags;			/* special flags */
 };
+#endif /*__ASSEMBLY__ */
 
 #endif /* __ASM_X86_SIGNAL_H */
--- include/asm-generic/signal.h
+++ include/asm-generic/signal.h
@@ -18,9 +18,9 @@
 #include <nucleos/types.h>
 
 #if defined(__KERNEL__) || defined(__UKERNEL__)
-#define _NSIG	26	/* highest signal number plus one */
+#define _NSIG	29	/* highest signal number plus one */
 #else
-#define NSIG	26
+#define NSIG	29
 #endif
 
 /* Here are types that are closely associated with signal handling. */
@@ -37,6 +37,7 @@ typedef unsigned long sigset_t;
 #define SIGILL		4	/* illegal instruction */
 #define SIGTRAP		5	/* trace trap (not reset when caught) */
 #define SIGABRT		6	/* IOT instruction */
+#define SIGIOT		6	/* IOT trap */
 #define SIGBUS		7	/* bus error */
 #define SIGFPE		8	/* floating point exception */
 #define SIGKILL		9	/* kill (cannot be caught or ignored) */
@@ -47,30 +48,21 @@ typedef unsigned long sigset_t;
 #define SIGALRM		14	/* alarm clock */
 #define SIGTERM		15	/* software termination signal from kill */
 #define SIGEMT		16	/* EMT instruction */
+#define SIGCLD		SIGCHLD	/* child process terminated or stopped */
 #define SIGCHLD		17	/* child process terminated or stopped */
-#define SIGWINCH	21	/* window size has changed */
-#define SIGVTALRM	24	/* virtual alarm */
-#define SIGPROF		25	/* profiler alarm */
-
-/* POSIX requires the following signals to be defined, even if they are
- * not supported.  Here are the definitions, but they are not supported.
- */
 #define SIGCONT		18	/* continue if stopped */
 #define SIGSTOP		19	/* stop signal */
 #define SIGTSTP		20	/* interactive stop signal */
+#define SIGWINCH	21	/* window size has changed */
 #define SIGTTIN		22	/* background process wants to read */
 #define SIGTTOU		23	/* background process wants to write */
+#define SIGVTALRM	24	/* virtual alarm */
+#define SIGPROF		25	/* profiler alarm */
+#define SIGXCPU		26	/* CPU limit exceeded (4.2 BSD) (n/a) */
+#define SIGXFSZ		27	/* File size limit exceeded (4.2 BSD) (n/a) */
 
-#define SIGIOT		SIGABRT	/* for people who speak PDP-11 */
-
-#if defined(__KERNEL__) || defined(__UKERNEL__)
-/* Nucleos specific signals. These signals are not used by user proceses, 
- * but meant to inform system processes, like the PM, about system events.
- */
-#define SIGKMESS	29	/* new kernel message */
-#define SIGKSIG		30	/* kernel signal pending */
-#define SIGNDELAY	31	/* kernel shutting down */
-#endif /* defined(__KERNEL__) || defined(__UKERNEL__) */
+#define SIGSYS		28	/* bad argument to a system call (n/a) */
+#define SIGUNUSED	28	/* purpose at present but is intended for future use (n/a) */
 
 /* Fields for sa_flags. */
 #define SA_ONSTACK	0x0001	/* deliver signal on alternate stack */
@@ -80,6 +72,15 @@ typedef unsigned long sigset_t;
 #define SA_SIGINFO	0x0010	/* extended signal handling */
 #define SA_NOCLDWAIT	0x0020	/* don't create zombies */
 #define SA_NOCLDSTOP	0x0040	/* don't receive SIGCHLD when child stops */
+
+#if defined(__KERNEL__) || defined(__UKERNEL__)
+/* Nucleos specific signals. These signals are not used by user proceses, 
+ * but meant to inform system processes, like the PM, about system events.
+ */
+#define SIGKMESS	29	/* new kernel message */
+#define SIGKSIG		30	/* kernel signal pending */
+#define SIGNDELAY	31	/* kernel shutting down */
+#endif /* defined(__KERNEL__) || defined(__UKERNEL__) */
 
 #include <asm-generic/signal-defs.h>
 
