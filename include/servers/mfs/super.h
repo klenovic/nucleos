a992463ab8917b016fc1fb3da84c47e9a2815ed7
Ladislav Klenovic <klenovic@nucleonsoft.com>
2010-01-25 12:04:04 +0100

    Don't export `struct buf' and white-space fixes

--- include/servers/mfs/super.h
+++ include/servers/mfs/super.h
@@ -30,42 +30,41 @@
  */
 
 struct super_block {
-  ino_t s_ninodes;		/* # usable inodes on the minor device */
-  zone1_t  s_nzones;		/* total device size, including bit maps etc */
-  short s_imap_blocks;		/* # of blocks used by inode bit map */
-  short s_zmap_blocks;		/* # of blocks used by zone bit map */
-  zone1_t s_firstdatazone;	/* number of first data zone */
-  short s_log_zone_size;	/* log2 of blocks/zone */
-  short s_pad;			/* try to avoid compiler-dependent padding */
-  off_t s_max_size;		/* maximum file size on this device */
-  zone_t s_zones;		/* number of zones (replaces s_nzones in V2) */
-  short s_magic;		/* magic number to recognize super-blocks */
+	ino_t s_ninodes;		/* # usable inodes on the minor device */
+	zone1_t  s_nzones;		/* total device size, including bit maps etc */
+	short s_imap_blocks;		/* # of blocks used by inode bit map */
+	short s_zmap_blocks;		/* # of blocks used by zone bit map */
+	zone1_t s_firstdatazone;	/* number of first data zone */
+	short s_log_zone_size;		/* log2 of blocks/zone */
+	short s_pad;			/* try to avoid compiler-dependent padding */
+	off_t s_max_size;		/* maximum file size on this device */
+	zone_t s_zones;			/* number of zones (replaces s_nzones in V2) */
+	short s_magic;			/* magic number to recognize super-blocks */
 
-  /* The following items are valid on disk only for V3 and above */
+	/* The following items are valid on disk only for V3 and above */
 
-  /* The block size in bytes. Minimum MIN_BLOCK SIZE. SECTOR_SIZE
-   * multiple. If V1 or V2 filesystem, this should be
-   * initialised to STATIC_BLOCK_SIZE.
-   */
-  short s_pad2;			/* try to avoid compiler-dependent padding */
-  unsigned short s_block_size;	/* block size in bytes. */
-  char s_disk_version;		/* filesystem format sub-version */
+	/* The block size in bytes. Minimum MIN_BLOCK SIZE. SECTOR_SIZE
+	 * multiple. If V1 or V2 filesystem, this should be
+	 * initialised to STATIC_BLOCK_SIZE.
+	 */
+	short s_pad2;			/* try to avoid compiler-dependent padding */
+	unsigned short s_block_size;	/* block size in bytes. */
+	char s_disk_version;		/* filesystem format sub-version */
 
-  /* The following items are only used when the super_block is in memory. */
-  
-  /*struct inode *s_isup;*/	/* inode for root dir of mounted file sys */
-  /*struct inode *s_imount;*/   /* inode mounted on */
-  unsigned s_inodes_per_block;	/* precalculated from magic number */
-  dev_t s_dev;			/* whose super block is this? */
-  int s_rd_only;		/* set to 1 iff file sys mounted read only */
-  int s_native;			/* set to 1 iff not byte swapped file system */
-  int s_version;		/* file system version, zero means bad magic */
-  int s_ndzones;		/* # direct zones in an inode */
-  int s_nindirs;		/* # indirect zones per indirect block */
-  bit_t s_isearch;		/* inodes below this bit number are in use */
-  bit_t s_zsearch;		/* all zones below this bit number are in use*/
-  char s_is_root;
-} superblock;
+	/* The following items are only used when the super_block is in memory. */
+	/*struct inode *s_isup;*/	/* inode for root dir of mounted file sys */
+	/*struct inode *s_imount;*/	/* inode mounted on */
+	unsigned s_inodes_per_block;	/* precalculated from magic number */
+	dev_t s_dev;			/* whose super block is this? */
+	int s_rd_only;		/* set to 1 iff file sys mounted read only */
+	int s_native;		/* set to 1 iff not byte swapped file system */
+	int s_version;		/* file system version, zero means bad magic */
+	int s_ndzones;		/* # direct zones in an inode */
+	int s_nindirs;		/* # indirect zones per indirect block */
+	bit_t s_isearch;	/* inodes below this bit number are in use */
+	bit_t s_zsearch;	/* all zones below this bit number are in use*/
+	char s_is_root;
+};
 
 #if defined(__KERNEL__) || defined(__UKERNEL__)
 extern struct super_block superblock;
--- include/servers/mfs/type.h
+++ include/servers/mfs/type.h
@@ -12,41 +12,43 @@
 
 /* Declaration of the V1 inode as it is on the disk (not in core). */
 typedef struct {		/* V1.x disk inode */
-  u16_t d1_mode;		/* file type, protection, etc. */
-  i16_t d1_uid;			/* user id of the file's owner */
-  i32_t d1_size;		/* current file size in bytes */
-  i32_t d1_mtime;		/* when was file data last changed */
-  u8_t d1_gid;			/* group number */
-  u8_t d1_nlinks;		/* how many links to this file */
-  u16_t d1_zone[V1_NR_TZONES];	/* block nums for direct, ind, and dbl ind */
+	u16_t d1_mode;		/* file type, protection, etc. */
+	i16_t d1_uid;		/* user id of the file's owner */
+	i32_t d1_size;		/* current file size in bytes */
+	i32_t d1_mtime;		/* when was file data last changed */
+	u8_t d1_gid;		/* group number */
+	u8_t d1_nlinks;		/* how many links to this file */
+	u16_t d1_zone[V1_NR_TZONES];	/* block nums for direct, ind, and dbl ind */
 } d1_inode;
 
 /* Declaration of the V2 inode as it is on the disk (not in core). */
 typedef struct {		/* V2.x disk inode */
-  u16_t d2_mode;		/* file type, protection, etc. */
-  u16_t d2_nlinks;		/* how many links to this file. HACK! */
-  i16_t d2_uid;			/* user id of the file's owner. */
-  u16_t d2_gid;			/* group number HACK! */
-  i32_t d2_size;		/* current file size in bytes */
-  i32_t d2_atime;		/* when was file data last accessed */
-  i32_t d2_mtime;		/* when was file data last changed */
-  i32_t d2_ctime;		/* when was inode data last changed */
-  zone_t d2_zone[V2_NR_TZONES];	/* block nums for direct, ind, and dbl ind */
+	u16_t d2_mode;		/* file type, protection, etc. */
+	u16_t d2_nlinks;	/* how many links to this file. HACK! */
+	i16_t d2_uid;		/* user id of the file's owner. */
+	u16_t d2_gid;		/* group number HACK! */
+	i32_t d2_size;		/* current file size in bytes */
+	i32_t d2_atime;		/* when was file data last accessed */
+	i32_t d2_mtime;		/* when was file data last changed */
+	i32_t d2_ctime;		/* when was inode data last changed */
+	zone_t d2_zone[V2_NR_TZONES];	/* block nums for direct, ind, and dbl ind */
 } d2_inode;
 
+#if defined (__KERNEL__) || defined(__UKERNEL__)
 struct buf {
-  /* Data portion of the buffer. */
-  union fsdata_u *bp;
+	/* Data portion of the buffer. */
+	union fsdata_u *bp;
 
-  /* Header portion of the buffer. */
-  struct buf *b_next;           /* used to link all free bufs in a chain */
-  struct buf *b_prev;           /* used to link all free bufs the other way */
-  struct buf *b_hash;           /* used to link bufs on hash chains */
-  block_t b_blocknr;            /* block number of its (minor) device */
-  dev_t b_dev;                  /* major | minor device where block resides */
-  char b_dirt;                  /* CLEAN or DIRTY */
-  char b_count;                 /* number of users of this buffer */
-  int b_bytes;                  /* Number of bytes allocated in bp */
+	/* Header portion of the buffer. */
+	struct buf *b_next;	/* used to link all free bufs in a chain */
+	struct buf *b_prev;	/* used to link all free bufs the other way */
+	struct buf *b_hash;	/* used to link bufs on hash chains */
+	block_t b_blocknr;	/* block number of its (minor) device */
+	dev_t b_dev;		/* major | minor device where block resides */
+	char b_dirt;		/* CLEAN or DIRTY */
+	char b_count;		/* number of users of this buffer */
+	int b_bytes;		/* Number of bytes allocated in bp */
 };
+#endif /* defined (__KERNEL__) || defined(__UKERNEL__) */
 
 #endif /* __SERVERS_MFS_TYPE_H */
