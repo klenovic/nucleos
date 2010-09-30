ccc64d68c667f69557d29888f4b72e93a8fe13a8
Ladislav Klenovic <klenovic@nucleonsoft.com>
2010-09-30 11:26:08 +0200

    Minor source code clean-up and reorganization

--- kernel/panic.c
+++ kernel/panic.c
@@ -9,8 +9,6 @@
  */
 /* This file contains a collection of miscellaneous procedures:
  *   kernel_panic: abort Nucleos due to a fatal error
- *   kprintf: (from lib/sysutil/kprintf.c)
- *   kputc:  buffered putc used by kernel kprintf
  */
 #include <nucleos/unistd.h>
 #include <nucleos/signal.h>
@@ -40,44 +38,4 @@ void kernel_panic(char *mess, int nr)
 
 	/* Abort Nucleos. */
 	nucleos_shutdown(NULL);
-}
-
-
-/* Include system printf() implementation named kprintf() */
-
-#define printf kprintf
-#include "../lib/sysutil/kprintf.c"
-
-void kputc(int c)
-{
-	/* Accumulate a single character for a kernel message. Send a notification
-	 * to the output driver if an END_OF_KMESS is encountered.
-	 */
-	if (c != END_OF_KMESS) {
-		if (do_serial_debug) {
-			if(c == '\n')
-				ser_putc('\r');
-
-			ser_putc(c);
-		}
-
-		kmess.km_buf[kmess.km_next] = c;	/* put normal char in buffer */
-
-		if (kmess.km_size < sizeof(kmess.km_buf))
-			kmess.km_size += 1;
-
-		kmess.km_next = (kmess.km_next + 1) % KMESS_BUF_SIZE;
-	} else {
-		int p, outprocs[] = OUTPUT_PROCS_ARRAY;
-
-		if(!(kernel_in_panic || do_serial_debug)) {
-			for(p = 0; outprocs[p] != ENDPT_NONE; p++) {
-				if(isokprocn(outprocs[p]) && !isemptyn(outprocs[p])) {
-					send_sig(outprocs[p], SIGKMESS);
-				}
-			}
-		}
-	}
-
-	return;
 }
--- kernel/printk.c
+++ kernel/printk.c
@@ -11,18 +11,53 @@
  *								15 Jan 1994
  */
 #include <stdarg.h>
+#include <nucleos/signal.h>
 #include <nucleos/kernel.h>
 #include <nucleos/types.h>
 #include <nucleos/stddef.h>
+#include <nucleos/com.h>
 #include <nucleos/limits.h>
+#include <kernel/kernel.h>
+#include <kernel/proc.h>
 
 #define isdigit(c)	((unsigned) ((c) - '0') <  (unsigned) 10)
 #define count_kputc(c) do { charcount++; kputc(c); } while(0)
 
-/* Printf() uses kputc() to print characters. */
-void kputc(int c);
+void kputc(int c)
+{
+	/* Accumulate a single character for a kernel message. Send a notification
+	 * to the output driver if an END_OF_KMESS is encountered.
+	 */
+	if (c != END_OF_KMESS) {
+		if (do_serial_debug) {
+			if(c == '\n')
+				ser_putc('\r');
 
-int printf(const char *fmt, ...)
+			ser_putc(c);
+		}
+
+		kmess.km_buf[kmess.km_next] = c;	/* put normal char in buffer */
+
+		if (kmess.km_size < sizeof(kmess.km_buf))
+			kmess.km_size += 1;
+
+		kmess.km_next = (kmess.km_next + 1) % KMESS_BUF_SIZE;
+	} else {
+		int p, outprocs[] = OUTPUT_PROCS_ARRAY;
+
+		if(!(kernel_in_panic || do_serial_debug)) {
+			for(p = 0; outprocs[p] != ENDPT_NONE; p++) {
+				if(isokprocn(outprocs[p]) && !isemptyn(outprocs[p])) {
+					send_sig(outprocs[p], SIGKMESS);
+				}
+			}
+		}
+	}
+
+	return;
+}
+
+int kprintf(const char *fmt, ...)
 {
 	int c, charcount = 0;
 	enum { LEFT, RIGHT } adjust;
