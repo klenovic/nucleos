905e7212095fa52c83f895f4194c8313be940c4e
Ladislav Klenovic <klenovic@nucleonsoft.com>
2011-05-15 11:07:06 +0200

    Minor coding style fixes

--- arch/x86/kernel/do_int86.c
+++ arch/x86/kernel/do_int86.c
@@ -24,29 +24,25 @@
 
 struct reg86u reg86;
 
-/*===========================================================================*
- *				do_int86					     *
- *===========================================================================*/
-int do_int86(m_ptr)
-register kipc_msg_t *m_ptr;	/* pointer to request message */
+int do_int86(kipc_msg_t *m_ptr)
 {
-  data_copy(who_e, (vir_bytes) m_ptr->INT86_REG86,
-	SYSTEM, (vir_bytes) &reg86, sizeof(reg86));
+	data_copy(who_e, (vir_bytes) m_ptr->INT86_REG86,SYSTEM, (vir_bytes)&reg86,
+		  sizeof(reg86));
 
-  level0(int86);
+	level0(int86);
 
-  /* Copy results back to the caller */
-  data_copy(SYSTEM, (vir_bytes) &reg86,
-	who_e, (vir_bytes) m_ptr->INT86_REG86, sizeof(reg86));
+	/* Copy results back to the caller */
+	data_copy(SYSTEM, (vir_bytes) &reg86, who_e, (vir_bytes)m_ptr->INT86_REG86,
+		  sizeof(reg86));
 
-  /* The BIOS call eats interrupts. Call get_randomness to generate some
-   * entropy. Normally, get_randomness is called from an interrupt handler.
-   * Figuring out the exact source is too complicated. CLOCK_IRQ is normally
-   * not very random.
-   */
-  lock;
-  get_randomness(&krandom, CLOCK_IRQ);
-  unlock;
+	/* The BIOS call eats interrupts. Call get_randomness to generate some
+	 * entropy. Normally, get_randomness is called from an interrupt handler.
+	 * Figuring out the exact source is too complicated. CLOCK_IRQ is normally
+	 * not very random.
+	 */
+	lock;
+	get_randomness(&krandom, CLOCK_IRQ);
+	unlock;
 
-  return 0;
+	return 0;
 }
--- arch/x86/kernel/do_readbios.c
+++ arch/x86/kernel/do_readbios.c
@@ -11,28 +11,24 @@
  *   m_type:	SYS_READBIOS
  *
  * The parameters for this kernel call are:
- *    m_data1:	RDB_SIZE		number of bytes to copy
- *    m_data4:	RDB_ADDR		absolute address in BIOS area
- *    m_data6:	RDB_BUF			buffer address in requesting process
+ *   message->RDB_SIZE		number of bytes to copy
+ *   message->RDB_ADDR		absolute address in BIOS area
+ *   message->RDB_BUF		buffer address in requesting process
  */
 
 #include <kernel/system.h>
 #include <nucleos/type.h>
 
-/*===========================================================================*
- *				do_readbios				     *
- *===========================================================================*/
-int do_readbios(m_ptr)
-register kipc_msg_t *m_ptr;	/* pointer to request message */
+int do_readbios(kipc_msg_t *m_ptr)
 {
-  struct vir_addr src, dst;     
-        
-  src.segment = BIOS_SEG;
-  dst.segment = D;
-  src.offset = m_ptr->RDB_ADDR;
-  dst.offset = (vir_bytes) m_ptr->RDB_BUF;
-  src.proc_nr_e = ENDPT_NONE;
-  dst.proc_nr_e = m_ptr->m_source;      
+	struct vir_addr src, dst;
 
-  return virtual_copy_vmcheck(&src, &dst, m_ptr->RDB_SIZE);
+	src.segment = BIOS_SEG;
+	dst.segment = D;
+	src.offset = m_ptr->RDB_ADDR;
+	dst.offset = (vir_bytes) m_ptr->RDB_BUF;
+	src.proc_nr_e = ENDPT_NONE;
+	dst.proc_nr_e = m_ptr->m_source;
+
+	return virtual_copy_vmcheck(&src, &dst, m_ptr->RDB_SIZE);
 }
