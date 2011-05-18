c76e084e57d7f5640840cce9381a6023fa5b5bfd
Ladislav Klenovic <klenovic@nucleonsoft.com>
2011-05-18 09:05:55 +0200

    Remove useless headers, no K&R function style

--- arch/x86/servers/vm/pagefaults.c
+++ arch/x86/servers/vm/pagefaults.c
@@ -7,36 +7,12 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, version 2 of the License.
  */
-#include <nucleos/unistd.h>
-#include <nucleos/com.h>
-#include <nucleos/const.h>
-#include <servers/ds/ds.h>
-#include <nucleos/endpoint.h>
-#include <nucleos/keymap.h>
-#include <nucleos/minlib.h>
+#include <nucleos/types.h>
 #include <nucleos/type.h>
-#include <nucleos/kipc.h>
-#include <nucleos/sysutil.h>
+#include <nucleos/endpoint.h>
 #include <nucleos/syslib.h>
-#include <nucleos/safecopies.h>
-#include <nucleos/bitmap.h>
-#include <nucleos/errno.h>
-#include <nucleos/string.h>
-#include <env.h>
-#include <stdio.h>
-#include <nucleos/fcntl.h>
 
-#include <servers/vm/glo.h>
-#include <servers/vm/proto.h>
-#include <servers/vm/util.h>
-
-/*===========================================================================*
- *				arch_handle_pagefaults	     		     *
- *===========================================================================*/
-int arch_get_pagefault(who, addr, err)
-endpoint_t *who;
-vir_bytes *addr;
-u32_t *err;
+int arch_get_pagefault(endpoint_t *who, vir_bytes *addr, u32 *err)
 {
-	return sys_vmctl_get_pagefault_i386(who, (u32_t*)addr, err);
+	return sys_vmctl_get_pagefault_i386(who, (u32*)addr, err);
 }
