c765d8da5821afa40189909d134d3bbfb9f66bbe
Ladislav Klenovic <klenovic@nucleonsoft.com>
2011-08-18 15:28:59 +0200

    Port Linux string functions.

--- include/nucleos/string.h
+++ include/nucleos/string.h
@@ -1,54 +1,131 @@
-/*
- *  Copyright (C) 2011  Ladislav Klenovic <klenovic@nucleonsoft.com>
- *
- *  This file is part of Nucleos kernel.
- *
- *  Nucleos kernel is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, version 2 of the License.
- */
-/* The <nucleos/string.h> header contains prototypes for the string handling 
- * functions.
- */
-#ifndef __NUCLEOS_STRING_H
-#define __NUCLEOS_STRING_H
+#ifndef _NUCLEOS_STRING_H
+#define _NUCLEOS_STRING_H
+
+/* We don't want strings.h stuff being used by user stuff by accident */
 
 #if !defined(__KERNEL__) && !defined(__UKERNEL__)
-
 #include <string.h>
-
 #else
 
-#include <nucleos/compiler.h>
-#include <nucleos/types.h>
-#include <nucleos/stddef.h>
+#include <nucleos/compiler.h>	/* for inline */
+#include <nucleos/types.h>	/* for size_t */
+#include <nucleos/stddef.h>	/* for NULL */
 #include <stdarg.h>
 
-/* Function Prototypes. */
-void *memchr(const void *_s, int _c, __kernel_size_t _n);
-int memcmp(const void *_s1, const void *_s2, __kernel_size_t _n);
-void *memcpy(void *_s1, const void *_s2, __kernel_size_t _n);
-void *memmove(void *_s1, const void *_s2, __kernel_size_t _n);
-void *memset(void *_s, int _c, __kernel_size_t _n);
-char *strcat(char *_s1, const char *_s2);
-char *strchr(const char *_s, int _c);
-int strncmp(const char *_s1, const char *_s2, __kernel_size_t _n);
-int strcmp(const char *_s1, const char *_s2);
-int strcoll(const char *_s1, const char *_s2);
-char *strcpy(char *_s1, const char *_s2);
-__kernel_size_t strcspn(const char *_s1, const char *_s2);
-char *strerror(int _errnum);
-__kernel_size_t strlen(const char *_s);
-char *strncat(char *_s1, const char *_s2, __kernel_size_t _n);
-char *strncpy(char *_s1, const char *_s2, __kernel_size_t _n);
-char *strpbrk(const char *_s1, const char *_s2);
-char *strrchr(const char *_s, int _c);
+extern char *strndup_user(const char __user *, long);
+extern void *memdup_user(const void __user *, size_t);
 
-/* Misc. extra functions */
-int strcasecmp(const char *_s1, const char *_s2);
-int strncasecmp(const char *_s1, const char *_s2, __kernel_size_t _len);
-__kernel_size_t strnlen(const char *_s, __kernel_size_t _n);
+/*
+ * Include machine specific inline routines
+ */
+#include <asm/string.h>
 
-#endif /*  defined(__KERNEL__) || defined(__UKERNEL__) */
+#ifndef __HAVE_ARCH_STRCPY
+extern char * strcpy(char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNCPY
+extern char * strncpy(char *,const char *, __kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_STRLCPY
+size_t strlcpy(char *, const char *, size_t);
+#endif
+#ifndef __HAVE_ARCH_STRCAT
+extern char * strcat(char *, const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNCAT
+extern char * strncat(char *, const char *, __kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_STRLCAT
+extern size_t strlcat(char *, const char *, __kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_STRCMP
+extern int strcmp(const char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNCMP
+extern int strncmp(const char *,const char *,__kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_STRNICMP
+extern int strnicmp(const char *, const char *, __kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_STRCASECMP
+extern int strcasecmp(const char *s1, const char *s2);
+#endif
+#ifndef __HAVE_ARCH_STRNCASECMP
+extern int strncasecmp(const char *s1, const char *s2, size_t n);
+#endif
+#ifndef __HAVE_ARCH_STRCHR
+extern char * strchr(const char *,int);
+#endif
+#ifndef __HAVE_ARCH_STRNCHR
+extern char * strnchr(const char *, size_t, int);
+#endif
+#ifndef __HAVE_ARCH_STRRCHR
+extern char * strrchr(const char *,int);
+#endif
+extern char * __must_check skip_spaces(const char *);
 
-#endif /* __NUCLEOS_STRING_H */
+extern char *strim(char *);
+
+static inline __must_check char *strstrip(char *str)
+{
+	return strim(str);
+}
+
+#ifndef __HAVE_ARCH_STRSTR
+extern char * strstr(const char *, const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNSTR
+extern char * strnstr(const char *, const char *, size_t);
+#endif
+#ifndef __HAVE_ARCH_STRLEN
+extern __kernel_size_t strlen(const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNLEN
+extern __kernel_size_t strnlen(const char *,__kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_STRPBRK
+extern char * strpbrk(const char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRSEP
+extern char * strsep(char **,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRSPN
+extern __kernel_size_t strspn(const char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRCSPN
+extern __kernel_size_t strcspn(const char *,const char *);
+#endif
+
+#ifndef __HAVE_ARCH_MEMSET
+extern void * memset(void *,int,__kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_MEMCPY
+extern void * memcpy(void *,const void *,__kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_MEMMOVE
+extern void * memmove(void *,const void *,__kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_MEMSCAN
+extern void * memscan(void *,int,__kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_MEMCMP
+extern int memcmp(const void *,const void *,__kernel_size_t);
+#endif
+#ifndef __HAVE_ARCH_MEMCHR
+extern void * memchr(const void *,int,__kernel_size_t);
+#endif
+
+extern bool sysfs_streq(const char *s1, const char *s2);
+extern int strtobool(const char *s, bool *res);
+
+/**
+ * strstarts - does @str start with @prefix?
+ * @str: string to examine
+ * @prefix: prefix to look for.
+ */
+static inline bool strstarts(const char *str, const char *prefix)
+{
+	return strncmp(str, prefix, strlen(prefix)) == 0;
+}
+#endif
+#endif /* _NUCLEOS_STRING_H */
