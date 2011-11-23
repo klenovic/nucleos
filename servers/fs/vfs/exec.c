93f253026ea745a10011784d503f41f7dec12cfc
Ladislav Klenovic <klenovic@nucleonsoft.com>
2011-11-23 16:54:15 +0100

    Don't allocate more than 1024 bytes on stack.
    	* The array of size 4096 (_MAX_BLOCK_SIZE) is
    	  now allocated on heap.
    	* Re-organize functions' order.

--- servers/fs/vfs/exec.c
+++ servers/fs/vfs/exec.c
@@ -7,6 +7,7 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, version 2 of the License.
  */
+#include <stdlib.h>
 #include <nucleos/kernel.h>
 #include "fs.h"
 #include <nucleos/stat.h>
@@ -30,12 +31,196 @@
 #include "vnode.h"
 #include "vmnt.h"
 
-static int patch_stack(struct vnode *vp, char stack[ARG_MAX], vir_bytes *stk_bytes);
-static int insert_arg(char stack[ARG_MAX], vir_bytes *stk_bytes, char *arg, int replace);
-static void patch_ptr(char stack[ARG_MAX], vir_bytes base);
-static void clo_exec(struct fproc *rfp);
-
 #define PTRSIZE		sizeof(char *)	/* Size of pointers in argv[] and envp[]. */
+
+/**
+ * Patch the stack
+ * @param stack  pointer to stack image within VFS
+ * @param base  virtual address of stack base inside user
+ */
+static void patch_ptr(char *stack, vir_bytes base)
+{
+/* When doing an exec(name, argv, envp) call, the user builds up a stack
+ * image with arg and env pointers relative to the start of the stack.  Now
+ * these pointers must be relocated, since the stack is not positioned at
+ * address 0 in the user's address space.
+ */
+
+	char **ap, flag;
+	vir_bytes v;
+
+	flag = 0;			/* counts number of 0-pointers seen */
+	ap = (char **) stack;		/* points initially to 'nargs' */
+	ap++;				/* now points to argv[0] */
+
+	while (flag < 2) {
+		if (ap >= (char **) &stack[ARG_MAX]) return;	/* too bad */
+
+		if (*ap != NULL) {
+			v = (vir_bytes) *ap;	/* v is relative pointer */
+			v += base;		/* relocate it */
+			*ap = (char *) v;	/* put it back */
+		} else {
+			flag++;
+		}
+
+		ap++;
+	}
+}
+
+/**
+ * Patch the stack
+ * @param stack  pointer to stack image within PM
+ * @param stk_bytes  size of initial stack
+ * @param replace  argument to prepend/replace as new argv[0]
+ * @return True iff the operation succeeded.
+ */
+static int insert_arg(char *stack, vir_bytes *stk_bytes, char *arg, int replace)
+{
+/* Patch the stack so that arg will become argv[0].  Be careful, the stack may
+ * be filled with garbage, although it normally looks like this:
+ * nargs argv[0] ... argv[nargs-1] NULL envp[0] ... NULL
+ * followed by the strings "pointed" to by the argv[i] and the envp[i].  The
+ * pointers are really offsets from the start of stack.
+ * Return true iff the operation succeeded.
+ */
+	int offset, a0, a1, old_bytes = *stk_bytes;
+
+	/* Prepending arg adds at least one string and a zero byte. */
+	offset = strlen(arg) + 1;
+
+	a0 = (int) ((char **) stack)[1];	/* argv[0] */
+
+	if (a0 < 4 * PTRSIZE || a0 >= old_bytes) return(FALSE);
+
+	a1 = a0;	/* a1 will point to the strings to be moved */
+
+	if (replace) {
+		/* Move a1 to the end of argv[0][] (argv[1] if nargs > 1). */
+		do {
+			if (a1 == old_bytes) return(FALSE);
+			--offset;
+		} while (stack[a1++] != 0);
+	} else {
+		offset += PTRSIZE;	/* new argv[0] needs new pointer in argv[] */
+		a0 += PTRSIZE;		/* location of new argv[0][]. */
+	}
+
+	/* stack will grow by offset bytes (or shrink by -offset bytes) */
+	if ((*stk_bytes += offset) > ARG_MAX) return(FALSE);
+
+	/* Reposition the strings by offset bytes */
+	memmove(stack + a1 + offset, stack + a1, old_bytes - a1);
+
+	strcpy(stack + a0, arg);	/* Put arg in the new space. */
+
+	if (!replace) {
+		/* Make space for a new argv[0]. */
+		memmove(stack + 2 * PTRSIZE, stack + 1 * PTRSIZE, a0 - 2 * PTRSIZE);
+
+		((char **) stack)[0]++;	/* nargs++; */
+	}
+
+	/* Now patch up argv[] and envp[] by offset. */
+	patch_ptr(stack, (vir_bytes) offset);
+
+	((char **) stack)[1] = (char *) a0;	/* set argv[0] correctly */
+
+	return(TRUE);
+}
+
+/**
+ * Patch the argument vector
+ * @param vp  pointer for open script file
+ * @param stack  pointer to stack image within VFS
+ * @param stk_bytes  size of initial stack
+ * @return The path name of the interpreter.
+ */
+static int patch_stack(struct vnode *vp, char *stack, vir_bytes *stk_bytes)
+{
+/* Patch the argument vector to include the path name of the script to be
+ * interpreted, and all strings on the #! line.  Returns the path name of
+ * the interpreter.
+ */
+	enum { INSERT=FALSE, REPLACE=TRUE };
+	int n, r;
+	off_t pos;
+	char *sp, *interp = NULL;
+	u64_t new_pos;
+	unsigned int cum_io_incr;
+	char *buf;
+
+	buf = malloc(_MAX_BLOCK_SIZE);
+
+	/* Make user_fullpath the new argv[0]. */
+	if (!insert_arg(stack, stk_bytes, user_fullpath, REPLACE)) return(-ENOMEM);
+
+	pos = 0;	/* Read from the start of the file */
+
+	/* Issue request */
+	r = req_readwrite(vp->v_fs_e, vp->v_inode_nr, cvul64(pos),
+			  READING, VFS_PROC_NR, buf, _MAX_BLOCK_SIZE, &new_pos, &cum_io_incr);
+
+	if (r != 0) return r;
+
+	n = vp->v_size;
+
+	if (n > _MAX_BLOCK_SIZE)
+		n = _MAX_BLOCK_SIZE;
+
+	if (n < 2) return -ENOEXEC;
+
+	sp = &(buf[2]);				/* just behind the #! */
+	n -= 2;
+
+	if (n > PATH_MAX) n = PATH_MAX;
+
+	/* Use the user_fullpath variable for temporary storage */
+	memcpy(user_fullpath, sp, n);
+
+	if ((sp = memchr(user_fullpath, '\n', n)) == NULL) /* must be a proper line */
+		return(-ENOEXEC);
+
+	/* Move sp backwards through script[], prepending each string to stack. */
+	for (;;) {
+		/* skip spaces behind argument. */
+		while (sp > user_fullpath && (*--sp == ' ' || *sp == '\t'));
+
+		if (sp == user_fullpath) break;
+
+		sp[1] = 0;
+
+		/* Move to the start of the argument. */
+		while (sp > user_fullpath && sp[-1] != ' ' && sp[-1] != '\t') --sp;
+
+		interp = sp;
+
+		if (!insert_arg(stack, stk_bytes, sp, INSERT)) return(-ENOMEM);
+	}
+
+	/* Round *stk_bytes up to the size of a pointer for alignment contraints. */
+	*stk_bytes= ((*stk_bytes + PTRSIZE - 1) / PTRSIZE) * PTRSIZE;
+
+	if (interp != user_fullpath)
+		memmove(user_fullpath, interp, strlen(interp)+1);
+
+	return 0;
+}
+
+/**
+ * @brief Handle close on exec
+ * @param rfp  pointer to fproc structure
+ */
+static void clo_exec(struct fproc *rfp)
+{
+/* Files can be marked with the FD_CLOEXEC bit (in fp->fp_cloexec). */
+	int i;
+
+	/* Check the file desriptors one by one for presence of FD_CLOEXEC. */
+	for (i = 0; i < OPEN_MAX; i++)
+		if (FD_ISSET(i, &rfp->fp_cloexec_set))
+			close_fd(rfp, i);
+}
 
 /**
  * @brief Perform the execve(name, argv, envp) call
@@ -58,9 +243,9 @@ int pm_exec(int proc_e, char *path, vir_bytes path_len, char *frame, vir_bytes f
 	struct vnode *vp;
 	char *cp;
 	struct kstat ksb;
-	static char mbuf[ARG_MAX];	/* buffer for stack and zeroes */
 	struct nucleos_binprm bfmt_param;
 	struct nucleos_binfmt *bhandler;
+	static char mbuf[ARG_MAX];	/* buffer for stack and zeroes */
 
 	okendpt(proc_e, &proc_s);
 
@@ -224,188 +409,3 @@ int pm_exec(int proc_e, char *path, vir_bytes path_len, char *frame, vir_bytes f
 	return 0;
 }
 
-/*===========================================================================*
- *				patch_stack				     *
- *===========================================================================*/
-static int patch_stack(vp, stack, stk_bytes)
-struct vnode *vp;		/* pointer for open script file */
-char stack[ARG_MAX];		/* pointer to stack image within VFS_PROC_NR */
-vir_bytes *stk_bytes;		/* size of initial stack */
-{
-/* Patch the argument vector to include the path name of the script to be
- * interpreted, and all strings on the #! line.  Returns the path name of
- * the interpreter.
- */
-	enum { INSERT=FALSE, REPLACE=TRUE };
-	int n, r;
-	off_t pos;
-	char *sp, *interp = NULL;
-	u64_t new_pos;
-	unsigned int cum_io_incr;
-	char buf[_MAX_BLOCK_SIZE];
-
-	/* Make user_fullpath the new argv[0]. */
-	if (!insert_arg(stack, stk_bytes, user_fullpath, REPLACE)) return(-ENOMEM);
-
-	pos = 0;	/* Read from the start of the file */
-
-	/* Issue request */
-	r = req_readwrite(vp->v_fs_e, vp->v_inode_nr, cvul64(pos),
-			  READING, VFS_PROC_NR, buf, _MAX_BLOCK_SIZE, &new_pos, &cum_io_incr);
-
-	if (r != 0) return r;
-	
-	n = vp->v_size;
-
-	if (n > _MAX_BLOCK_SIZE)
-		n = _MAX_BLOCK_SIZE;
-
-	if (n < 2) return -ENOEXEC;
-	
-	sp = &(buf[2]);				/* just behind the #! */
-	n -= 2;
-
-	if (n > PATH_MAX) n = PATH_MAX;
-
-	/* Use the user_fullpath variable for temporary storage */
-	memcpy(user_fullpath, sp, n);
-
-	if ((sp = memchr(user_fullpath, '\n', n)) == NULL) /* must be a proper line */
-		return(-ENOEXEC);
-
-	/* Move sp backwards through script[], prepending each string to stack. */
-	for (;;) {
-		/* skip spaces behind argument. */
-		while (sp > user_fullpath && (*--sp == ' ' || *sp == '\t'));
-
-		if (sp == user_fullpath) break;
-
-		sp[1] = 0;
-
-		/* Move to the start of the argument. */
-		while (sp > user_fullpath && sp[-1] != ' ' && sp[-1] != '\t') --sp;
-
-		interp = sp;
-
-		if (!insert_arg(stack, stk_bytes, sp, INSERT)) return(-ENOMEM);
-	}
-
-	/* Round *stk_bytes up to the size of a pointer for alignment contraints. */
-	*stk_bytes= ((*stk_bytes + PTRSIZE - 1) / PTRSIZE) * PTRSIZE;
-
-	if (interp != user_fullpath)
-		memmove(user_fullpath, interp, strlen(interp)+1);
-
-	return 0;
-}
-
-/*===========================================================================*
- *				insert_arg				     *
- *===========================================================================*/
-static int insert_arg(stack, stk_bytes, arg, replace)
-char stack[ARG_MAX];		/* pointer to stack image within PM */
-vir_bytes *stk_bytes;		/* size of initial stack */
-char *arg;			/* argument to prepend/replace as new argv[0] */
-int replace;
-{
-/* Patch the stack so that arg will become argv[0].  Be careful, the stack may
- * be filled with garbage, although it normally looks like this:
- * nargs argv[0] ... argv[nargs-1] NULL envp[0] ... NULL
- * followed by the strings "pointed" to by the argv[i] and the envp[i].  The
- * pointers are really offsets from the start of stack.
- * Return true iff the operation succeeded.
- */
-	int offset, a0, a1, old_bytes = *stk_bytes;
-
-	/* Prepending arg adds at least one string and a zero byte. */
-	offset = strlen(arg) + 1;
-
-	a0 = (int) ((char **) stack)[1];	/* argv[0] */
-
-	if (a0 < 4 * PTRSIZE || a0 >= old_bytes) return(FALSE);
-
-	a1 = a0;	/* a1 will point to the strings to be moved */
-
-	if (replace) {
-		/* Move a1 to the end of argv[0][] (argv[1] if nargs > 1). */
-		do {
-			if (a1 == old_bytes) return(FALSE);
-			--offset;
-		} while (stack[a1++] != 0);
-	} else {
-		offset += PTRSIZE;	/* new argv[0] needs new pointer in argv[] */
-		a0 += PTRSIZE;		/* location of new argv[0][]. */
-	}
-
-	/* stack will grow by offset bytes (or shrink by -offset bytes) */
-	if ((*stk_bytes += offset) > ARG_MAX) return(FALSE);
-
-	/* Reposition the strings by offset bytes */
-	memmove(stack + a1 + offset, stack + a1, old_bytes - a1);
-
-	strcpy(stack + a0, arg);	/* Put arg in the new space. */
-
-	if (!replace) {
-		/* Make space for a new argv[0]. */
-		memmove(stack + 2 * PTRSIZE, stack + 1 * PTRSIZE, a0 - 2 * PTRSIZE);
-
-		((char **) stack)[0]++;	/* nargs++; */
-	}
-
-	/* Now patch up argv[] and envp[] by offset. */
-	patch_ptr(stack, (vir_bytes) offset);
-
-	((char **) stack)[1] = (char *) a0;	/* set argv[0] correctly */
-
-	return(TRUE);
-}
-
-/*===========================================================================*
- *				patch_ptr				     *
- *===========================================================================*/
-static void patch_ptr(stack, base)
-char stack[ARG_MAX];		/* pointer to stack image within VFS_PROC_NR */
-vir_bytes base;			/* virtual address of stack base inside user */
-{
-/* When doing an exec(name, argv, envp) call, the user builds up a stack
- * image with arg and env pointers relative to the start of the stack.  Now
- * these pointers must be relocated, since the stack is not positioned at
- * address 0 in the user's address space.
- */
-
-	char **ap, flag;
-	vir_bytes v;
-
-	flag = 0;			/* counts number of 0-pointers seen */
-	ap = (char **) stack;		/* points initially to 'nargs' */
-	ap++;				/* now points to argv[0] */
-
-	while (flag < 2) {
-		if (ap >= (char **) &stack[ARG_MAX]) return;	/* too bad */
-
-		if (*ap != NULL) {
-			v = (vir_bytes) *ap;	/* v is relative pointer */
-			v += base;		/* relocate it */
-			*ap = (char *) v;	/* put it back */
-		} else {
-			flag++;
-		}
-
-		ap++;
-	}
-}
-
-/**
- * @brief Handle close on exec
- * @param rfp  pointer to fproc structure
- */
-static void clo_exec(struct fproc *rfp)
-{
-/* Files can be marked with the FD_CLOEXEC bit (in fp->fp_cloexec). */
-	int i;
-
-	/* Check the file desriptors one by one for presence of FD_CLOEXEC. */
-	for (i = 0; i < OPEN_MAX; i++)
-		if (FD_ISSET(i, &rfp->fp_cloexec_set))
-			close_fd(rfp, i);
-}
